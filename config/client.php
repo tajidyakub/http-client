0b306ee6a72f59b184b7710a1278f465ec76d48f
Tajid Yakub <tajid.yakub@gmail.com>
2022-01-07 04:12:48 +0700

    Modify return array

--- config/client.php
+++ config/client.php
@@ -13,7 +13,7 @@ return [
         CURLOPT_SSL_VERIFYPEER => false,
         CURLOPT_SSL_VERIFYHOST => 0,
         CURLOPT_AUTOREFERER => true,
-        // CURLOPT_TCP_FASTOPEN => true,
+        CURLOPT_TCP_FASTOPEN => true,
         CURLOPT_TCP_KEEPALIVE => 1,	
         CURLOPT_DNS_SERVERS => '127.0.0.1,1.1.1.1,1.0.0.1',
         CURLOPT_ACCEPT_ENCODING => '',
