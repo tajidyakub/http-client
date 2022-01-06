c4311b16a190a72db216a631838b7bc38f790444
Tajid Yakub <tajid.yakub@gmail.com>
2022-01-07 00:07:16 +0700

    Add local DNS Server

--- src/HttpClient.php
+++ src/HttpClient.php
@@ -141,6 +141,8 @@ class HttpClient
         $this->request->set('time', time());
         $this->applyHeaders();
 
+        dd(curl_getinfo($this->client));
+
         $payload = [];
         
         try
